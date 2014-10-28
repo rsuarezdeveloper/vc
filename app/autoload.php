272c4ad0ec586ebe1bf006da3c674f3f66332e76
rsuarezdeveloper <jrsc22@gmail.com>
2014-10-27 20:33:46 -0500

    Cambios Actuales

--- app/autoload.php
+++ app/autoload.php
@@ -7,7 +7,7 @@ use Composer\Autoload\ClassLoader;
 * @var $loader ClassLoader
 */
 $loader = require __DIR__.'/../vendor/autoload.php';
-require_once  __DIR__.'/../vendor/mpdf/src/mpdf.php';
+//require_once  __DIR__.'/../vendor/mpdf/src/mpdf.php';
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
